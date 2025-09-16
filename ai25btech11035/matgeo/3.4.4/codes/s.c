2a30157ecb5ce4c3f415350cfaaec23a209337d3
root <root@localhost.localdomain>
2025-09-16 16:16:05 +0000

    commit

---
+++ ai25btech11035/matgeo/3.4.4/codes/s.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Rectangle vertices
+    int Ax = 0, Ay = 0;
+    int Bx = 0, By = 5;
+    int Cx = 3, Cy = 5;
+    int Dx = 3, Dy = 0;
+
+    printf("Coordinates of the rectangle are:\n");
+    printf("A(%d, %d)\n", Ax, Ay);
+    printf("B(%d, %d)\n", Bx, By);
+    printf("C(%d, %d)\n", Cx, Cy);
+    printf("D(%d, %d)\n", Dx, Dy);
+
+    return 0;
+}
