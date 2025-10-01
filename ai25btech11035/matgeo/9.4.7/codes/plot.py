980130fb4805d09890c7fc03776e187112bb420a
root <root@localhost.localdomain>
2025-10-01 17:30:43 +0000

    commit

--- ai25btech11035/matgeo/9.4.7/codes/plot.py
+++ ai25btech11035/matgeo/9.4.7/codes/plot.py
@@ -3,31 +3,34 @@ import matplotlib.pyplot as plt
 
 # Define the quadratic function
 def quadratic(x):
-    return 16*x**2 + 8*x + 1
+    return 16*x**2 - 8*x + 1
 
-# Generate x values for the parabola
-x_values = np.linspace(-1, 0.5, 400)
+# Generate x values for the parabola plot
+x_values = np.linspace(0, 0.5, 400)
 y_values = quadratic(x_values)
 
-# Roots of the quadratic equation
-roots_x = [0.25, 0.25]
-roots_y = [0, 0]
+# Root of the quadratic (double root)
+root_x = 0.25
+root_y = 0  # quadratic(root_x) is zero
 
-# Plot the parabola
-plt.plot(x_values, y_values, label='Parabola: 16x^2 + 8x + 1')
+# Plot the parabola curve
+plt.plot(x_values, y_values, label='Parabola: 16x^2 - 8x + 1')
 
-# Mark the roots on the x-axis
-plt.scatter(roots_x, roots_y, color='red', label='Roots')
+# Mark the root(s)
+plt.scatter(root_x, root_y, color='red', label='Root at x=0.25')
 
-# Plot x-axis line
-plt.axhline(0, color='black', linewidth=0.7)
+# Draw x-axis line
+plt.axhline(y=0, color='black', linewidth=0.7)
 
-# Set labels and title
+# Labels and title
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Parabola and Roots of the Quadratic Equation')
+plt.title('Parabola and Root of the Quadratic Equation')
 
+# Legend and grid
 plt.legend()
 plt.grid(True)
+
+# Show plot
 plt.savefig('../figs/img.png')
 plt.show()
